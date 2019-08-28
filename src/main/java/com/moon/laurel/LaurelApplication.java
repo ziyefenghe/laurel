93b0c816983ba98aec33521c165544bd7f43deaa
ziyefenghe <ziyefenghe@gmail.com>
2019-08-28 17:45:38 +0800

    初始化项目

---
+++ src/main/java/com/moon/laurel/LaurelApplication.java
@@ -0,0 +1,13 @@
+package com.moon.laurel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaurelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaurelApplication.class, args);
+    }
+
+}
